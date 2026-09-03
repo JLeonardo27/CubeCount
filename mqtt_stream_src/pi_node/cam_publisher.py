f2170fe53c7ce17014d960da34a852cf72c9e16f
JLeonardo27 <elsupercellid@gmail.com>
2026-09-02 19:34:37 -0600

    Fix

--- mqtt_stream_src/pi_node/cam_publisher.py
+++ mqtt_stream_src/pi_node/cam_publisher.py
@@ -21,7 +21,7 @@ class VideoPublisher(MQTTClientBase):
 def main():
     publisher = VideoPublisher("PiZero_Cam")
     
-    cap = cv2.VideoCapture(0)
+    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
     
     cap.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
     cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
